f9be948d4a6e07e535d981952b5c989703c43f2e
miguelbarrosv <miguel.barros@ikasle.egibide.org>
2020-02-20 12:40:41 +0100

    index casi hecho

--- app/Http/Controllers/PreguntaController.php
+++ app/Http/Controllers/PreguntaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Pregunta;
 use App\rc;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class PreguntaController extends Controller
@@ -16,13 +17,25 @@ class PreguntaController extends Controller
      */
     public function index()
     {
-        $preguntasUsuario = DB::table('preguntas')
-            ->join('usuarios', 'usuarios.id', '=', 'preguntas.id_usuario')
-            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'usuarios.nombre')
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
             ->paginate(10);
 
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
         return view('index', [
-            'preguntas' => $preguntasUsuario
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
         ]);
     }
 
@@ -90,5 +103,150 @@ class PreguntaController extends Controller
     public function destroy(rc $rc)
     {
         //
+    }
+
+    public function semana() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->where('preguntas.created_at', '>', Carbon::now()->startOfWeek())
+            ->where('preguntas.created_at', '<', Carbon::now()->endOfWeek())
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
+    }
+
+    public function dia() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->whereDate('preguntas.created_at', Carbon::today())
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
+    }
+
+    public function mes() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->whereMonth('preguntas.created_at', '=', Carbon::now()->month)
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
+    }
+
+    public function populares() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->orderBy('preguntas.visita','DESC')
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
+    }
+
+    public function reciente() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->orderBy('preguntas.id','DESC')
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
+    }
+
+    public function activas() {
+        $preguntas = DB::table('preguntas')
+            ->join('users', 'users.id', '=', 'preguntas.id_usuario')
+            ->select('preguntas.titulo','preguntas.id','preguntas.visita','preguntas.updated_at','preguntas.etiquetas', 'preguntas.descripcion', 'preguntas.estado', 'users.nombre')
+            ->where('preguntas.estado','=', 1)
+            ->paginate(10);
+
+        $respuestas = DB::table('respuestas')
+            ->select(DB::raw('count(*) as numPreguntas, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        $votos = DB::table('votos')
+            ->select(DB::raw('count(*) as numVotos, id_pregunta'))
+            ->groupBy('id_pregunta')
+            ->get();
+
+        return view('index', [
+            'preguntas' => $preguntas,
+            'respuestas' =>$respuestas,
+            'votos' => $votos
+        ]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,53 +5,64 @@
             <div class="main">
                 <div>
                     <h2 class="mb-3 float-left">Preguntas más recientes</h2>
-                    <a class="float-right btn btn-primary" href="/preguntas/formular" data-value="active">Hacer una pregunta</a>
+                        <a class="float-right btn btn-primary" href="{{ route('pregunta.create')}} " data-value="active">Hacer una pregunta</a>
                 </div>
 
                 <ul class="w-100 nav nav-tabs" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#home">Activas</a>
+                        <a class="nav-link"  href="{{route('index.reciente')}}">Mas recientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#">Mas recientes</a>
+                        <a class="nav-link "  href="/activas">Activas</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#">Populares</a>
+                        <a class="nav-link"  href="/populares">Populares</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#">Hoy</a>
+                        <a class="nav-link"  href="/hoy">Hoy</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#">Semana</a>
+                        <a class="nav-link"  href="/semana">Semana</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#">Mes</a>
+                        <a class="nav-link"  href="/mes">Mes</a>
                     </li>
                 </ul>
             </div>
-            <div class="lista-preguntas row border-bottom p-2">
+
                 @foreach($preguntas as $pregunta)
-                    <div class="d-flex col-5" onclick="window.location.href='preguntas/{{$pregunta->id}}'">
-                        <div class="votos text-center  col-4">
-                            <span class="col-12"></span>
-                            <span>Votos</span>
+                <div class="pregunta row border-bottom p-3 ">
+                        <div class="d-flex col-5" onclick="window.location.href='preguntas/{{$pregunta->id}}'">
+                            <div class="votos text-center  col-4">
+                                @foreach($votos as $voto)
+                                    @if($pregunta->id = $voto->id_pregunta)
+                                        <span class="col-12">{{$voto->numVotos}}</span>
+                                    @endif
+                                @endforeach
+                                <span class="col-12">Votos</span>
+                            </div>
+                            <div class="respuestas text-center  col-4">
+                                @foreach($respuestas as $respuesta)
+                                    @if($pregunta->id = $respuesta->id_pregunta)
+                                        <span class="col-12">{{$respuesta->numPreguntas}}</span>
+                                    @endif
+                                @endforeach
+                                <span>Respuestas</span>
+                            </div>
+                            <div class="visitas text-center col-4 ">
+                                <span class="col-12">{{$pregunta->visita}}</span>
+                                <span class="col-12">Visitas</span>
+                            </div>
                         </div>
-                        <div class="respuestas text-center  col-4">
-                            <span>Respuestas</span>
+                        <div class="col-5 ">
+                            <h3><a href="preguntas/{{$pregunta->id}}" class="w-100">{{$pregunta->titulo}}</a></h3>
+                            <div class="etiquetas float-left">
+                                <mark class="p-1">{{json_decode($pregunta->etiquetas)->a}}</mark>
+                            </div>
+                            <span class="float-right ">creada por <a href="">{{$pregunta->nombre}}</a></span>
                         </div>
-                        <div class="visitas text-center col-4">
-                            <span class="col-12">{{$pregunta->visita}}</span>
-                            <span class="col-12">Visitas</span>
-                        </div>
-                    </div>
-                    <div class="col-5">
-                        <h3><a href="preguntas/{{$pregunta->id}}" class="w-100">{{$pregunta->titulo}}</a></h3>
-                        <div class="etiquetas">
-                            {{$pregunta->etiquetas}}
-                        </div>
-                        <span>modificada hace {{$pregunta->updated_at}} <a>{{$pregunta->nombre}}</a></span>
-                    </div>
+                </div>
                 @endforeach
-            </div>
+            {{$preguntas->links()}}
          </div>
     @endsection
