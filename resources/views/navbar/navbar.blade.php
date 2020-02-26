1fe2f0e2044b3e89b7175cc6a3ac6ed18b3c516e
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-02-26 10:10:20 +0100

    arreglo bug

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -19,8 +19,6 @@
         </div>
     </form>
 
-    <img src="{{Storage::url( Auth::user()->url_foto)}}" id="user_img">
-    <input type="hidden" value="{{ Auth::user()->id }}" id="user_id">
     <!-- Right Side Of Navbar -->
     <ul class="navbar-nav m-auto  d-flex flex-row">
         <!-- Authentication Links -->
@@ -34,6 +32,9 @@
                 </li>
             @endif
         @else
+            <img src="{{Storage::url( Auth::user()->url_foto)}}" id="user_img">
+            <input type="hidden" value="{{ Auth::user()->id }}" id="user_id">
+
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false" v-pre>
