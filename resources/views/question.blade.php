82b9fd4eccb28b00b5afecd2a92e784336a61cb9
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-02-26 12:56:42 +0100

    ic--> Dar quitar favorito

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.layout')
 
 @section('content')
-<p>{{$pregunta}}</p>
-
+    <p>{{$pregunta}}</p>
+    <div id="fav-container">
+        <!-- comprobar si esta pregunta es favorita o no, y poner un icono u otro-->
+        <input hidden id="idUsuario" value="{{Auth::user()->id}}">
+        <input hidden id="idPregunta" value="{{$pregunta->id}}">
+    </div>
+    <script src="{{secure_asset('js/favoritos.js')}}"></script>
 @endsection
