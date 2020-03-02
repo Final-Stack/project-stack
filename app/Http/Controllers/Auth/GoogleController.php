1f9088436a5093b7290a80841491c5ff4ca0e077
iñaki-home <inaki.caballero@egibide.org>
2020-03-02 16:23:09 +0100

    ic--> Login google?

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use Exception;
