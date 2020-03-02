f5ab2eb79b9767c85af943e3f7c6facbe2f99a84
iñaki-home <inaki.caballero@egibide.org>
2020-03-02 16:05:30 +0100

    h

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -31,11 +31,11 @@ class GoogleController extends Controller
 
             $user = Socialite::driver('google')->user();
 
-            $finduser = User::where('google_id', $user->id)->first();
+            $existUser = User::where('email',$user->email)->first();
 
-            if ($finduser) {
+            if ($existUser) {
 
-                Auth::login($finduser);
+                Auth::login($existUser);
 
                 return redirect()->route('index');
 
